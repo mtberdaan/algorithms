ddf9d25fade17d2fb4ee366509e165dcb5e92134
Daan <daandenhoed@gmail.com>
2024-08-01 10:48:14 +0200

    iterators

--- java/Queue.java
+++ java/Queue.java
@@ -35,7 +35,24 @@ public class Queue<Item> implements Iterable<Item>
         return item;
     }
 
-    // iterator
+    public Iterator<Item> iterator()
+    { return new ListIterator(); }
+
+    private class ListIterator implements Iterator<Item>
+    {
+        private Node current = first;
+
+        public boolean hasNext()
+        { return current != null; }
+
+        public void remove() {}
+        
+        public Item next()
+        {
+            Item item = current.item;
+            current = current.next;
+            return item;
+    }
     // test client main
 }
 
--- java/Stack.java
+++ java/Stack.java
@@ -31,8 +31,25 @@ public class Stack<Item> implements Iterable<Item>
         return item; // return old item
     }
     
-    // iterator()
-    // test client main()
+    public Iterator<Item> iterator()
+    { return new ListIterator(); }
+
+    private class ListIterator implements Iterator<Item>
+    {
+        private Node current = first;
+
+        public boolean hasNext()
+        { return current != null; }
+
+        public void remove() {}
+        
+        public Item next()
+        {
+            Item item = current.item;
+            current = current.next;
+            return item;
+    }
+    // test client main
 }
 
 
